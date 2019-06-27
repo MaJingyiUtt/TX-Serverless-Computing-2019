06d9f3fdf88a7ee4ca5bb6810b3a59fb066caffd
MaJingyiUtt <48433363+MaJingyiUtt@users.noreply.github.com>
2019-06-27 15:46:19 +0200

    edit readme for S3 and Lambda

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
 # TX-Serverless-Computing-2019
+Amazon Simple Storage Service(S3)
+=================================
+There are two S3 buckets in this project, one is 'tx-picture' for all the original images, another is 'tx-vignette' for thumbnails. 
+To create a bucket is simple, in the console, click on Create a bucket, enter the bucket name then click create. 
+
+![](./images/create-bucket.PNG)
+
+Then in the bucket, edit Block public access settings under the Tab Permissions.
+
+![](./images/s3-public-access.PNG)
+
+We also need to edit the bucket policy. 
+
+>`{
+    "Version": "2012-10-17",
+    "Id": "Policy1557411810501",
+    "Statement": [
+        {
+            "Sid": "Stmt1557411804169",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::user-number:user/upload-s3"
+            },
+            "Action": [
+                "s3:PutObject",
+                "s3:PutObjectAcl",
+                "s3:GetObject",
+                "s3:GetObjectAcl",
+                "s3:DeleteObject"
+            ],
+            "Resource": "arn:aws:s3:::tx-picture/*"
+        }
+    ]
+}`
+
+![](./images/bucket-policy.PNG)
+
+Lambda function
+===============
+Create a IAM role for Lambda
+----------------------------
+In the console of IAM, choose Create role. 
+The service of the role is Lambda. 
+
+![](./images/create-role-1.PNG)
+
+In the next step, choose all the policies we need : CloudWatchLogsReadOnlyAccess, AmazonS3FullAccess and AmazonDynamoDBFullAccess. For DynamoDb, we need to specifiy the table name. 
+
+![](./images/create-role-2.PNG)
+
+Then review the role and create. 
+
+![](./images/create-role-4.PNG)
+
+Create Lambda function
+----------------------
+Create a lambda function as following. Use the role that we've created before. 
+
+![](./images/create-lambda.PNG)
+
+After the function created, add a trigger of S3 to Lambda. 
+
+![](./images/add-trigger.PNG)
+
+Set time out to 30 secondes. 
+
+![](./images/set-timeout.PNG)
+
+Then upload the zip file of the folder [lambda function](./lambda-function). Please make sure that you zip all the files in the folder but not the folder itself. 
--- lambda-function/index.js
+++ lambda-function/index.js
@@ -1,4 +1,4 @@
-exports.handler = async(event, context) => {
+exports.handler = async (event, context) => {
     console.log('Loading function');
     const aws = require('aws-sdk');
     const s3 = new aws.S3({ apiVersion: '2006-03-01' });
@@ -10,13 +10,14 @@ exports.handler = async(event, context) => {
         Bucket: bucket,
         Key: key,
     };
-        const { ETag } = await s3.getObject(params).promise();
+    /*Hash of the image*/
+    const { ETag } = await s3.getObject(params).promise();
     var etag = ETag.split("\"")[1];
     /*get data of the image*/
     const data = await s3.getObject(params).promise();
-console.log(data.Body);
+    console.log(data.Body);
     /*write a temp file, attention, sync*/
-    fs.writeFileSync('/tmp/image', data.Body, function(err) {
+    fs.writeFileSync('/tmp/image', data.Body, function (err) {
         if (err) {
             console.log(err.code, "-", err.message);
         }
@@ -24,39 +25,38 @@ console.log(data.Body);
             console.log("file saved");
         }
     });
-     var process = require('child_process');
-     var type = process.execSync('file --mime-type /tmp/image').toString();
-         type = type.split(" ")[1];
+    /*Run linux commands*/
+    var process = require('child_process');
+    /*Type and encoding of the image*/
+    var type = process.execSync('file --mime-type /tmp/image').toString();
+    type = type.split(" ")[1];
     var encoding = process.execSync('file --mime-encoding /tmp/image').toString();
+    /*Date time original and GPS position of the image*/
     var date = process.execSync('perl exiftool -DateTimeOriginal -d %Y-%m-%d -q -s2 /tmp/image').toString();
-    date=date.split(" ")[1];
+    date = date.split(" ")[1];
     encoding = encoding.split(" ")[1];
     var gps = process.execSync('perl exiftool -GPSPosition -q -s2 /tmp/image').toString();
-    gps=gps.split(": ")[1];
-     console.log(type, date,encoding, gps);
+    gps = gps.split(": ")[1];
+    /*Make a thumbnail*/
     process.execSync('./anytopnm /tmp/image|./pamscale -height 100|./pnmtojpeg > /tmp/tmpImage');
+    /*Get the thumbnail ant put it in to the S3 bucket*/
     var vignette = fs.readFileSync('/tmp/tmpImage');
     console.log(vignette);
-  //  var base = new Buffer(contents.toString(), 'binary').toString('base64');
-    //console.log("base64 = "+base);
-    // var convertedBuffer = Buffer.from(base, 'base64');
-//console.log(convertedBuffer);
-
     var vParams = {
         Bucket: "tx-vignette",
-        Key: etag+'.jpg',
+        Key: etag + '.jpg',
         Body: vignette
     };
     await s3.putObject(vParams).promise();
-    
-     var dBparams = {
+    /*Data to stock in the database*/
+    var dBparams = {
         Item: {
-            filename:key,
+            filename: key,
             etag
         },
         TableName: 'SteatiteName'
     };
-     await docClient.put(dBparams).promise();
+    await docClient.put(dBparams).promise();
     var dBmetaparams = {
         Item: {
             etag,
@@ -67,6 +67,5 @@ console.log(data.Body);
         },
         TableName: 'SteatiteMeta'
     };
-
     await docClient.put(dBmetaparams).promise();
 };
